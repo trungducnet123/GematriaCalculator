1fc6f7b5d6eb75d8c49a665a64c6d66c3dc54563
MediaZ DEV <116047433+trungducnet123@users.noreply.github.com>
2023-11-07 15:19:13 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ siteMetadata: {
     titleTemplate: "%s · Gematria Calculator",
     description:
       "Gematria or gimatria is a system of assigning numerical value to a word or phrase. Hebrew Gematria calculator is a tool to help in this task. Moreover you can find the correspondences of the Liber Sepher Sephiroth sub figurâ D",
-    url: "https://gematria.starryabode.com", // No trailing slash allowed!
+    url: "https://thunderous-pastelito-24239a.netlify.app", // No trailing slash allowed!
     image: "src/images/icon512.png" // Path to your image you placed in the 'static' folder
   },
 plugins: [
