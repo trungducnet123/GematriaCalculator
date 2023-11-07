cc2714ad112f36ddc55d9061a7ef6eee6fa1432a
MediaZ DEV <116047433+trungducnet123@users.noreply.github.com>
2023-11-07 15:49:19 +0700

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ export default () => (
   <Layout>
     <h1>I'd love to talk! Email me at the address below</h1>
     <p>
-      <a href="mailto:me@example.com">me@example.com</a>
+      <a href="mailto:contact@kabastro.com">contact@kabastro.com</a>
     </p>
   </Layout>
 )
